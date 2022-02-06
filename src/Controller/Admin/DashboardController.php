24918f7b87915a42de9a307ba1793bd3bce58e26
ali <aramzan.pro@gmail.com>
2022-02-06 13:14:37 +0100

    set easyadmin and crud

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Entity\Product;
+use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Heavytaild Shop');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home', Product::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Product', 'fas fa-tshirt', Product::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,351 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $details;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isBestSeller= false;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isNewArrival= false;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isFeatured= false;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isSpecialOffer= false;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Picture;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Category::class, inversedBy="products")
+     */
+    private $category;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=TagsProduct::class, mappedBy="product")
+     */
+    private $tagsProducts;
+
+    /**
+     * @ORM\OneToMany(targetEntity=RelatedProduct::class, mappedBy="product")
+     */
+    private $relatedProducts;
+
+    /**
+     * @ORM\OneToMany(targetEntity=ReviewsProduct::class, mappedBy="product")
+     */
+    private $reviewsProducts;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Size::class, mappedBy="product")
+     */
+    private $sizes;
+
+    public function __construct()
+    {
+        $this->category = new ArrayCollection();
+        $this->tagsProducts = new ArrayCollection();
+        $this->relatedProducts = new ArrayCollection();
+        $this->reviewsProducts = new ArrayCollection();
+        $this->sizes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDetails(): ?string
+    {
+        return $this->details;
+    }
+
+    public function setDetails(?string $details): self
+    {
+        $this->details = $details;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getIsBestSeller(): ?bool
+    {
+        return $this->isBestSeller;
+    }
+
+    public function setIsBestSeller(bool $isBestSeller): self
+    {
+        $this->isBestSeller = $isBestSeller;
+
+        return $this;
+    }
+
+    public function getIsNewArrival(): ?bool
+    {
+        return $this->isNewArrival;
+    }
+
+    public function setIsNewArrival(bool $isNewArrival): self
+    {
+        $this->isNewArrival = $isNewArrival;
+
+        return $this;
+    }
+
+    public function getIsFeatured(): ?bool
+    {
+        return $this->isFeatured;
+    }
+
+    public function setIsFeatured(bool $isFeatured): self
+    {
+        $this->isFeatured = $isFeatured;
+
+        return $this;
+    }
+
+    public function getIsSpecialOffer(): ?bool
+    {
+        return $this->isSpecialOffer;
+    }
+
+    public function setIsSpecialOffer(bool $isSpecialOffer): self
+    {
+        $this->isSpecialOffer = $isSpecialOffer;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->Picture;
+    }
+
+    public function setPicture(string $Picture): self
+    {
+        $this->Picture = $Picture;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategory(): Collection
+    {
+        return $this->category;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->category->contains($category)) {
+            $this->category[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        $this->category->removeElement($category);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|TagsProduct[]
+     */
+    public function getTagsProducts(): Collection
+    {
+        return $this->tagsProducts;
+    }
+
+    public function addTagsProduct(TagsProduct $tagsProduct): self
+    {
+        if (!$this->tagsProducts->contains($tagsProduct)) {
+            $this->tagsProducts[] = $tagsProduct;
+            $tagsProduct->addProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTagsProduct(TagsProduct $tagsProduct): self
+    {
+        if ($this->tagsProducts->removeElement($tagsProduct)) {
+            $tagsProduct->removeProduct($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|RelatedProduct[]
+     */
+    public function getRelatedProducts(): Collection
+    {
+        return $this->relatedProducts;
+    }
+
+    public function addRelatedProduct(RelatedProduct $relatedProduct): self
+    {
+        if (!$this->relatedProducts->contains($relatedProduct)) {
+            $this->relatedProducts[] = $relatedProduct;
+            $relatedProduct->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRelatedProduct(RelatedProduct $relatedProduct): self
+    {
+        if ($this->relatedProducts->removeElement($relatedProduct)) {
+            // set the owning side to null (unless already changed)
+            if ($relatedProduct->getProduct() === $this) {
+                $relatedProduct->setProduct(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|ReviewsProduct[]
+     */
+    public function getReviewsProducts(): Collection
+    {
+        return $this->reviewsProducts;
+    }
+
+    public function addReviewsProduct(ReviewsProduct $reviewsProduct): self
+    {
+        if (!$this->reviewsProducts->contains($reviewsProduct)) {
+            $this->reviewsProducts[] = $reviewsProduct;
+            $reviewsProduct->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReviewsProduct(ReviewsProduct $reviewsProduct): self
+    {
+        if ($this->reviewsProducts->removeElement($reviewsProduct)) {
+            // set the owning side to null (unless already changed)
+            if ($reviewsProduct->getProduct() === $this) {
+                $reviewsProduct->setProduct(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Size[]
+     */
+    public function getSizes(): Collection
+    {
+        return $this->sizes;
+    }
+
+    public function addSize(Size $size): self
+    {
+        if (!$this->sizes->contains($size)) {
+            $this->sizes[] = $size;
+            $size->addProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSize(Size $size): self
+    {
+        if ($this->sizes->removeElement($size)) {
+            $size->removeProduct($this);
+        }
+
+        return $this;
+    }
+}
