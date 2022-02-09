9a69a92650bbb06ba70109ccaf679959635af569
ali <aramzan.pro@gmail.com>
2022-02-09 20:39:57 +0100

    order entity

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -66,11 +66,6 @@ class Product
     private $category;
 
     /**
-     * @ORM\OneToMany(targetEntity=RelatedProduct::class, mappedBy="product")
-     */
-    private $relatedProducts;
-
-    /**
      * @ORM\OneToMany(targetEntity=ReviewsProduct::class, mappedBy="product")
      */
     private $reviewsProducts;
@@ -238,36 +233,6 @@ class Product
     public function removeCategory(Category $category): self
     {
         $this->category->removeElement($category);
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|RelatedProduct[]
-     */
-    public function getRelatedProducts(): Collection
-    {
-        return $this->relatedProducts;
-    }
-
-    public function addRelatedProduct(RelatedProduct $relatedProduct): self
-    {
-        if (!$this->relatedProducts->contains($relatedProduct)) {
-            $this->relatedProducts[] = $relatedProduct;
-            $relatedProduct->setProduct($this);
-        }
-
-        return $this;
-    }
-
-    public function removeRelatedProduct(RelatedProduct $relatedProduct): self
-    {
-        if ($this->relatedProducts->removeElement($relatedProduct)) {
-            // set the owning side to null (unless already changed)
-            if ($relatedProduct->getProduct() === $this) {
-                $relatedProduct->setProduct(null);
-            }
-        }
 
         return $this;
     }
